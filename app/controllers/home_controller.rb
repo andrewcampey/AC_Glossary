3141dc8a704cd6c22d74c087d0376b568a889299
Andrew Campey <andrew.campey@gmail.com>
2012-09-11 21:55:33 +1000

    json add attempt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,10 +10,11 @@ class HomeController < ApplicationController
   
   def addglossaryitem
     #@item = GlossaryItem.new(Term: params[:Term], Description: params[:Description])
-	@item = GlossaryItem.new(Term: "CHECKTest4", Description: "CHECKTest4")
-	@item.save
-    #respond_to do |format|
+	#@item = GlossaryItem.new(Term: "CHECKTest4", Description: "CHECKTest4")
+	#@item.save
+    respond_to do |format|
     #    format.json { render :json => @item.id }
-    #end
+	format.json { render :json => 1 }
+    end
   end
 end
