533f9a581474d4769d9fd4eafa2fe7df7682029f
Andrew Campey <andrew.campey@gmail.com>
2012-09-11 19:23:59 +1000

    Attempt to fix DB issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,12 @@ class HomeController < ApplicationController
 
   def login
   end
+  
+  def addglossaryitem
+    @item = GlossaryItem.new(Term: params[:Term], Description: params[:Description])
+
+    respond_to do |format|
+        format.json { render :json => @item.id }
+    end
+  end
 end
