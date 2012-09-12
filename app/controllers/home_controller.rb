3ff68cefef3dd6d4e3c49a5d0ea614d3c61300ee
Andrew Campey <andrew.campey@gmail.com>
2012-09-12 20:26:34 +1000

    Changed to use Rails ajax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,14 +8,14 @@ class HomeController < ApplicationController
   
   def addglossaryitem
     #@item = GlossaryItem.new(Term: params[:Term], Description: params[:Description])
-	@item = GlossaryItem.new(Term: "CHECKTest6", Description: "CHECKTest6")
+	@item = GlossaryItem.new(Term: "CHECKTest7", Description: "CHECKTest7")
 	@item.save
     #respond_to do |format|
     #    format.json { render :json => @item.id }
 	#format.js { 1 }
     #end
 	respond_to do |format|
-		format.js { id: 1 }
+		format.js { render text: "1" }
     end
   end
 end
